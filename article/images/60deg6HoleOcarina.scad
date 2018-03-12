b67222b70313845d9bf489a5923c3b9a5eee2118
TrucomanX <fernando.pujaico.rivera@gmail.com>
2018-03-12 00:27:41 -0300

    ordenando

---
+++ article/images/60deg6HoleOcarina.scad
@@ -0,0 +1,4 @@
+//everything in mm
+
+
+import("/media/fernando/FER8G-BTRFS/FORMULATION/OcarinaArticle/images/60deg6HoleOcarina.stl", convexity=3);
---
+++ article/make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+latex -interaction=nonstopmode ocarina6.tex
+latex -interaction=nonstopmode ocarina6.tex
+latex -interaction=nonstopmode ocarina6.tex
+
+./clean.sh

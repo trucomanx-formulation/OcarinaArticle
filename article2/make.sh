9733c92fe530a598dea34e18057e8f6f8b7b91a2
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2021-04-13 01:05:41 -0300

    Ordenando datos de bibliogrfia

--- article2/make.sh
+++ article2/make.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 pdflatex -synctex=1 -interaction=nonstopmode ocarina6.tex
-biber ocarina6
+bibtex ocarina6
 pdflatex -synctex=1 -interaction=nonstopmode ocarina6.tex
-biber ocarina6
+bibtex ocarina6
 pdflatex -synctex=1 -interaction=nonstopmode ocarina6.tex
 pdflatex -synctex=1 -interaction=nonstopmode ocarina6.tex
 
